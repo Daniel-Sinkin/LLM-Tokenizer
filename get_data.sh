1e75e35db1ea8ede1a8b521d016fd0fe87329331
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-03-14 16:14:23 +0100

    Minor update

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 echo "Getting 'input.txt' from the char-rnn Repo and saving it as 'data/shakespeare.txt'"
 if [ ! -d "data" ]; then
-    echo "data directory does not exist. Creating it."
+    echo "'data' directory does not exist. Creating it."
     mkdir -p "data"
 fi
 curl "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt" > data/shakespeare.txt
